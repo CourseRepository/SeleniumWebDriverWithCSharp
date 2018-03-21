50ac0a378d46d51e79541339ae371cda49c1cc8a
rathr1 <RahulSingh.Rathore@in.pega.com>
2018-03-21 18:33:48 +0530

    Submit form script

--- SeleniumWebdriver/ComponentHelper/ComboBoxHelper.cs
+++ SeleniumWebdriver/ComponentHelper/ComboBoxHelper.cs
@@ -5,12 +5,30 @@ using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
+using SeleniumWebdriver.Settings;
 
 namespace SeleniumWebdriver.ComponentHelper
 {
     public class ComboBoxHelper
     {
         private static SelectElement select;
+        private static WebDriverWait GetWebDriverWait(IWebDriver driver,TimeSpan timeout)
+        {
+            WebDriverWait wait = new WebDriverWait(driver,timeout)
+            {
+                PollingInterval = TimeSpan.FromMilliseconds(250)
+            };
+            wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+            return wait;
+        }
+
+        public static void SelectElementWitWait(By locator, int index)
+        {
+            WebDriverWait wait = GetWebDriverWait(ObjectRepository.Driver, TimeSpan.FromSeconds(60));
+            IWebElement element =  wait.Until(ExpectedConditions.ElementIsVisible(locator));
+            select = new SelectElement(element);
+            select.SelectByIndex(index);
+        }
 
         public static void SelectElement(By locator, int index)
         {
---
+++ SeleniumWebdriver/TestScript/Form/TestSubmitForm.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumWebdriver.ComponentHelper;
+using SeleniumWebdriver.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SeleniumWebdriver.TestScript.Form
+{
+    [TestClass]
+    public class TestSubmitForm
+    {
+        [TestMethod]
+        public void TestSubmit()
+        {
+            NavigationHelper.NavigateToUrl("https://www.w3schools.com/html/tryit.asp?filename=tryhtml_form_submit");
+            WebDriverWait wait = GetWait();
+            wait.Until(ExpectedConditions.FrameToBeAvailableAndSwitchToIt(OpenQA.Selenium.By.Id("iframeResult")));
+            TextBoxHelper.TypeInTextBox(By.CssSelector("[name='firstname']"), "Testing");
+            Thread.Sleep(2000);
+            Assert.AreEqual("MickeyTesting", GenericHelper.GetElement(By.CssSelector("[name='firstname']")).GetAttribute("value"));
+
+        }
+
+        private WebDriverWait GetWait()
+        {
+            WebDriverWait wait = new WebDriverWait(ObjectRepository.Driver, TimeSpan.FromSeconds(60))
+            {
+                PollingInterval = TimeSpan.FromMilliseconds(250)
+            };
+            return wait;
+        }
+    }
+}
