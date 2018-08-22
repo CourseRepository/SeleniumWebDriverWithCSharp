0a007a6d48ca3c85204505ae675d0547b382fae3
rathr1 <RahulSingh.Rathore@in.pega.com>
2018-08-22 17:25:45 +0530

    Extract text

---
+++ SeleniumWebdriver/TestScript/Question/TestGetDataFromPage.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using SeleniumWebdriver.ComponentHelper;
+using SeleniumWebdriver.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumWebdriver.TestScript.Question
+{
+    [TestClass]
+    public class TestGetDataFromPage
+    {
+        //Base xpath for the grid
+        private const string BaseXpathOfGrid = "//div[@id='approved']//div[@class='durandal-wrapper']//ol[@class='contentList medium ui-sortable']";
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            NavigationHelper.NavigateToUrl("https://internal.mem.com/sso/v2/internal");
+            GenericHelper.WaitForWebElementVisisble(By.Id("UserName"), TimeSpan.FromSeconds(60));
+            TextBoxHelper.TypeInTextBox(By.Id("UserName"), "AutoTester");
+            TextBoxHelper.TypeInTextBox(By.Id("Password"), "AutoTester123");
+            ButtonHelper.ClickButton(By.XPath("//span[text()='LOGIN']"));
+            GenericHelper.WaitForWebElementVisisble(By.XPath("//label[text()='MeM Internal']"), TimeSpan.FromSeconds(70));
+            ButtonHelper.ClickButton(By.XPath("//label[text()='MeM Internal']"));
+        }
+
+        [TestMethod]
+        public void TestGetData()
+        {
+            NavigationHelper.NavigateToUrl("https://admin.mem.com/sso/newde/7851114#contententry/condolences");
+            GenericHelper.WaitForWebElementVisisble(By.XPath(BaseXpathOfGrid), TimeSpan.FromSeconds(70));
+            var index = 2; // index for the item in un-ordered list
+            while(GenericHelper.IsElemetPresent(By.XPath(BaseXpathOfGrid + "//li[" + index + "]"))) // To check if there are more item present
+            {
+                IWebElement heading =  ObjectRepository.Driver.FindElement(By.XPath(BaseXpathOfGrid + "//li[" + index + "]//section//div[@class='thumbview']//div[@class='heading']")); // xpath of heading using index 2 -for 1st li , 3 - 2nd li...
+                Console.WriteLine(heading.Text);
+                heading = ObjectRepository.Driver.FindElement(By.XPath(BaseXpathOfGrid + "//li[" + index + "]//section//div[@class='thumbview']//div[@class='text']")); // xpath of description using index 2 -for 1st li , 3 - 2nd li...
+                Console.WriteLine(heading.Text);
+                index = index + 1;
+            }
+        }
+
+    }
+}
---
+++ SeleniumWebdriver/TestScript/Question/TestReadExcelFile.cs
@@ -0,0 +1,37 @@
+﻿using ExcelDataReader;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumWebdriver.TestScript.Question
+{
+    [TestClass]
+    public class TestReadExcelFile
+    {
+        [TestMethod]
+        public void TestReadExcel()
+        {
+            FileStream stream = new FileStream(@"C:\Users\rathr1\Desktop\KDD.xlsx", FileMode.Open, FileAccess.Read);
+            IExcelDataReader reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+            DataTable table = reader.AsDataSet().Tables["TC01"];
+
+            for(int i = 0; i < table.Rows.Count; i++)
+            {
+                var col = table.Rows[i];
+                for(int j = 0; j < col.ItemArray.Length; j++)
+                {
+                    Console.WriteLine(col.ItemArray[j]);
+                }
+            }
+
+            reader.Close();
+            stream.Close();
+
+        }
+    }
+}
