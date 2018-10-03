9371528521323efa1d3b87589496c883f2fe6281
rathr1 <RahulSingh.Rathore@in.pega.com>
2018-10-03 12:25:15 +0530

    Added Nlog code

---
+++ SeleniumWebdriver/TestScript/NLogScript/NLogExample.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SeleniumWebdriver.TestScript.NLogScript
+{
+    [TestClass]
+    public class NLogExample
+    {
+        private NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        [TestMethod]
+        public void TestNLogMethod()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Logger.Trace("Sample trace message");
+                Logger.Debug("Sample debug message");
+                Logger.Info("Sample informational message");
+                Logger.Warn("Sample warning message");
+                Logger.Error("Sample error message");
+                Logger.Fatal("Sample fatal error message");
+            }
+        }
+    }
+}
